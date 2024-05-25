e9f8bd4dc226c0be08c33791abb173d1ad5f1c73
jnn-jnn1 <anj72005@gmail.com>
2024-05-25 18:22:13 +0900

    글 목록 페이지 추가

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,9 +1,36 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function BoardList() {
+  const [boardList, setBoardList] = useState([]);
+  useEffect(() => {
+    axios.get("/api/board/list").then((res) => {
+      setBoardList(res.data);
+    });
+  }, []);
+
   return (
     <Box>
       <Box>게시물 목록</Box>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>#</Th>
+            <Th>작성자</Th>
+            <Th>작성일자</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {boardList.map((member) => (
+            <Tr key={member.id}>
+              <Td>{member.id}</Td>
+              <Td>{member.writer}</Td>
+              <Td>{member.dateAndTime}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   );
 }
