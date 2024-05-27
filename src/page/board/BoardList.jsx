d363fb36603fbdbdeb35a147d0b51a89b14a74eb
jnn-jnn1 <anj72005@gmail.com>
2024-05-28 00:39:57 +0900

    페이징 처리

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,16 +1,41 @@
-import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [pageInfo, setPageInfo] = useState({});
+
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => {
-      setBoardList(res.data);
+    axios.get(`/api/board/list?${searchParams}`).then((res) => {
+      setBoardList(res.data.boardList);
+      setPageInfo(res.data.pageInfo);
     });
-  }, []);
+  }, [searchParams]);
+
+  let pageNumbers = [];
+
+  for (let i = pageInfo.leftPageNumber; i < pageInfo.rightPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  function handlePage(pageNumber) {
+    searchParams.set("page", pageNumber);
+    navigate(`/?page=${pageNumber}`);
+  }
 
   return (
     <Box>
@@ -40,6 +65,45 @@ export function BoardList() {
           ))}
         </Tbody>
       </Table>
+      <Center>
+        {pageInfo.prevPageNumber && (
+          <>
+            <Button onClick={() => navigate("/?page=1")}>맨앞</Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}
+            >
+              이전
+            </Button>
+          </>
+        )}
+        <Box>
+          {pageNumbers.map((pageNumber) => (
+            <Button
+              colorScheme={
+                pageNumber === pageInfo.currentPageNumber ? "blue" : "gray"
+              }
+              key={pageNumber}
+              onClick={() => handlePage(pageNumber)}
+            >
+              {pageNumber}
+            </Button>
+          ))}
+        </Box>
+        {pageInfo.nextPageNumber && (
+          <>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.nextPageNumber}`)}
+            >
+              다음
+            </Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.lastPageNumber}`)}
+            >
+              맨끝
+            </Button>
+          </>
+        )}
+      </Center>
     </Box>
   );
 }
